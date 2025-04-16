dd321356482842b4ad2b54c9aac0872e615051c0
ProtocolSage <stuffy_linkup_0q@icloud.com>
2025-04-16 18:46:26 -0400

    Add Next.js and Tailwind configuration

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Configure MDX
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+}
+
+module.exports = nextConfig
